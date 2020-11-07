28fea2211ee880ac03855b576da89ab15c0c216d
Timmo <contact@timmo.xyz>
2020-11-07 23:47:57 +0000

    Cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,9 @@
 import React, { ReactElement } from "react";
 import { GetStaticProps } from "next";
-import Link from "next/link";
-import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
-import clsx from "clsx";
 
 import Layout from "../components/Layout";
 import Markdown from "../components/Markdown";
