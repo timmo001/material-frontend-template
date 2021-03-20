4e009ac146962e083a5f5b51c210dfc6d671a93d
Aidan Timson <contact@timmo.xyz>
2021-03-20 19:26:54 +0000

    Cleanup

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -59,7 +59,7 @@ function Layout(props: LayoutProps): ReactElement {
         }}
         color="transparent"
         fixed
-        rightLinks={<HeaderLinks {...props} />}
+        rightLinks={<HeaderLinks />}
       />
       {props.children}
       <Container className={classes.footer} component="footer" maxWidth="xl">
