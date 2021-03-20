302075a1cb455bf63a50b786c2aa6ec2fc580524
Aidan Timson <contact@timmo.xyz>
2021-03-20 19:28:38 +0000

    Rework MUI Setup (#69)
    * Rework mui setup
    
    * Update lockfile
    
    * Update lockfile
    
    * Cleanup

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,14 +1,6 @@
 import React, { ReactElement } from "react";
 import Head from "next/head";
-import { teal, indigo } from "@material-ui/core/colors";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import {
-  createMuiTheme,
-  responsiveFontSizes,
-  ThemeProvider,
-} from "@material-ui/core/styles";
 import { ClassNameMap } from "@material-ui/styles";
-import { NoSsr } from "@material-ui/core";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Container from "@material-ui/core/Container";
@@ -17,42 +9,6 @@ import Typography from "@material-ui/core/Typography";
 import Header from "./Header";
 import HeaderLinks from "./HeaderLinks";
 import Markdown from "./Markdown";
-
-let theme = createMuiTheme({
-  palette: {
-    type: "dark",
-    primary: teal,
-    secondary: indigo,
-    contrastThreshold: 3,
-    tonalOffset: 0.2,
-  },
-  overrides: {
-    MuiCard: {
-      root: {
-        margin: 8,
-      },
-    },
-    MuiCardContent: {
-      root: {
-        padding: "24px 32px",
-        "&:last-child": {
-          paddingBottom: 16,
-        },
-      },
-    },
-    MuiChip: {
-      root: {
-        margin: 4,
-      },
-    },
-    MuiCardActions: {
-      root: {
-        justifyContent: "flex-end",
-      },
-    },
-  },
-});
-theme = responsiveFontSizes(theme);
 
 interface LayoutProps {
   children?: ReactElement | ReactElement[];
@@ -74,27 +30,7 @@ function Layout(props: LayoutProps): ReactElement {
             ? `${props.title} - Material Frontend Template`
             : `Material Frontend Template`}
         </title>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#009688" />
         <link rel="canonical" href={props.url} />
-        <meta name="author" content="Aidan Timson" />
         <meta
           name="description"
           content={
@@ -113,39 +49,28 @@ function Layout(props: LayoutProps): ReactElement {
               : `material, frontend, template, material-ui, nextjs, reactjs, react, developer`
           }
         />
-        <meta name="msapplication-TileColor" content="#009688" />
-        <meta name="theme-color" content="#009688" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <NoSsr>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Header
-            {...props}
-            brand="Frontend"
-            changeColorOnScroll={{
-              height: 200,
-              color: "primary",
-            }}
-            color="transparent"
-            fixed
-            rightLinks={<HeaderLinks />}
-          />
-          {props.children}
-          <Container
-            className={classes.footer}
-            component="footer"
-            maxWidth="xl">
-            <Card>
-              <CardContent>
-                <Typography component="div">
-                  <Markdown source="Copyright © Owner" escapeHtml={false} />
-                </Typography>
-              </CardContent>
-            </Card>
-          </Container>
-        </ThemeProvider>
-      </NoSsr>
+      <Header
+        {...props}
+        brand="Frontend"
+        changeColorOnScroll={{
+          height: 200,
+          color: "primary",
+        }}
+        color="transparent"
+        fixed
+        rightLinks={<HeaderLinks />}
+      />
+      {props.children}
+      <Container className={classes.footer} component="footer" maxWidth="xl">
+        <Card>
+          <CardContent>
+            <Typography component="div">
+              <Markdown source="Copyright © Owner" escapeHtml={false} />
+            </Typography>
+          </CardContent>
+        </Card>
+      </Container>
     </>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,39 @@
-import { ReactElement, useEffect } from "react";
+import React, { ReactElement, useEffect } from "react";
 import { AppProps } from "next/app";
+import Head from "next/head";
+import { CssBaseline, ThemeProvider } from "@material-ui/core";
 
 import "assets/css/style.css";
 import "fontsource-roboto";
+import theme from "../components/Theme";
 
 function App({ Component, pageProps }: AppProps): ReactElement {
   useEffect(() => {
     document.documentElement.lang = "en-GB";
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
   }, []);
 
-  return <Component {...pageProps} />;
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />;
+
+  return (
+    <>
+      <Head>
+        <title>My page</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
